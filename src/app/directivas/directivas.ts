1e899ae903c02fd4a760ca74635c7c68572c07b9
Profesor Brian Vanegas <profe.brianvanegas@gmail.com>
2026-06-03 20:52:37 -0300

    Clase 3 de Junio: Directiva @For

--- src/app/directivas/directivas.ts
+++ src/app/directivas/directivas.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Mascota {
+  raza: string,
+  nombre: string,
+  edad: number
+}
+
 @Component({
   selector: 'app-directivas',
   imports: [],
@@ -9,4 +15,16 @@ export class Directivas {
   isLoggen: boolean = true;
 
   nombre: string = "Brian"
+
+  lenguajes: any[] = ['Python', 'PHP', 'JavaScript', 'TipeScript', 'JAVA', 'C#', 'C++'];
+
+  misMascotas: Mascota[] = [
+    {raza: 'Caballo', nombre: 'Tornado', edad: 50},
+    {raza: 'Labrador', nombre: 'Firulais', edad: 5},
+    {raza: 'Buldog', nombre: 'Bella', edad: 10},
+    {raza: 'Coballo', nombre: 'Charly', edad: 5},
+    {raza: 'Pitbull', nombre: 'Medel', edad: 8},
+    {raza: 'Dalmata', nombre: 'Manchas', edad: 5}
+  ]
+
 }
